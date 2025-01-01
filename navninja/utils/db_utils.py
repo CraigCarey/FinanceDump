806fe9324d1dc59860576e2e67afaa1a496daa5b
Craig Carey <sgccarey@gmail.com>
2025-01-01 15:43:56 +0000

    navninja stuff

--- navninja/utils/db_utils.py
+++ navninja/utils/db_utils.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
 import pandas as pd
 
 securities_db_file = "../data/securities_db.csv"
+holdings_db_file = "../data/holdings_db.csv"
+id_adjustments_db_file = "../data/id_adjustments_db.csv"
 
 
 def create_securities_db():
@@ -21,6 +25,9 @@ def create_securities_db():
 
 
 def get_securities_db():
+    if not Path(securities_db_file).exists():
+        return create_securities_db()
+
     securities_db = pd.read_csv(securities_db_file, keep_default_na=False)
     return securities_db
 
@@ -30,7 +37,6 @@ def write_securities_db(df):
 
 
 def create_holdings_db():
-
     holdings_db_cols = [
         "sedol",
         "quantity",
@@ -42,3 +48,43 @@ def create_holdings_db():
     holdings_db = pd.DataFrame(columns=holdings_db_cols)
 
     return holdings_db
+
+
+def get_holdings_db():
+    if not Path(holdings_db_file).exists():
+        return create_holdings_db()
+
+    holdings_db = pd.read_csv(holdings_db_file, keep_default_na=False)
+    return holdings_db
+
+
+def write_holdings_db(df):
+    df.to_csv(holdings_db_file, index=False)
+
+
+def create_id_adjustments_db():
+    cols = [
+        "type",
+        "trust",
+        "id_in",
+        "id_out",
+        "createdAt",
+        "updatedAt",
+        "deletedAt",
+    ]
+
+    db = pd.DataFrame(columns=cols)
+
+    return db
+
+
+def get_id_adjustments_db():
+    if not Path(id_adjustments_db_file).exists():
+        return create_id_adjustments_db()
+
+    id_adjustments_db = pd.read_csv(id_adjustments_db_file, keep_default_na=False)
+    return id_adjustments_db
+
+
+def write_id_adjustments_db(df):
+    df.to_csv(id_adjustments_db_file, index=False)
--- navninja/utils/openfigi_utils.py
+++ navninja/utils/openfigi_utils.py
@@ -14,29 +14,43 @@ def read_api_key(file_path):
         return None
 
 
-def query_openfigi_by_sedol(api_key, sedol):
+def query_openfigi(api_key, id_type, id_val):
     url = "https://api.openfigi.com/v3/mapping"
     headers = {"Content-Type": "application/json", "X-OPENFIGI-APIKEY": api_key}
-    payload = [{"idType": "ID_SEDOL", "idValue": sedol}]
+    payload = [{"idType": f"ID_{id_type}", "idValue": id_val}]
     try:
         response = requests.post(url, json=payload, headers=headers)
         response.raise_for_status()
         data = response.json()
+
+        if "warning" in data[0] or "error" in data[0]:
+            return None
+
         return data
+
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         return None
+
+
+def query_openfigi_by_sedol(api_key, sedol):
+    return query_openfigi(api_key, "SEDOL", sedol)
 
 
 def query_openfigi_by_isin(api_key, isin):
-    url = "https://api.openfigi.com/v3/mapping"
-    headers = {"Content-Type": "application/json", "X-OPENFIGI-APIKEY": api_key}
-    payload = [{"idType": "ID_ISIN", "idValue": isin}]
-    try:
-        response = requests.post(url, json=payload, headers=headers)
-        response.raise_for_status()
-        data = response.json()
-        return data
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None
+    return query_openfigi(api_key, "ISIN", isin)
+
+
+def figi_to_security(figi_data, sedol):
+    sedol_data = figi_data[0]["data"][0]
+    security = {
+        "sedol": sedol,
+        "figi": sedol_data["figi"],
+        "name": sedol_data["name"],
+        "ticker": sedol_data["ticker"],
+        "exchange": sedol_data["exchCode"],
+        "type": sedol_data["securityType"],
+        "sector": sedol_data["marketSector"],
+    }
+
+    return security
